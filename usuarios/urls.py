d339d41fd8336b49dd2f179b8afb113496d30379
Augusto Lopez <augustolopez674@gmail.com>
2023-07-13 22:24:13 -0300

    Añadí el avatar

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -8,5 +8,7 @@ app_name = 'usuarios'
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', LogoutView.as_view(template_name='usuarios/logout.html'), name='logout'),
-    path('registro/', views.registrarse, name='registrarse')
+    path('registro/', views.registrarse, name='registrarse'),
+    path('perfil/editar', views.editar_perfil, name='editar_perfil'),
+    path('perfil/editar/password/', views.ModificarPassword.as_view(), name='modificar_password')
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.views import PasswordChangeView
 from django.contrib.auth import authenticate, login as app_login
-from usuarios.forms import FormularioCreacionUsuarios
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from usuarios.forms import FormularioCreacionUsuarios, FormularioEditarDatos
+from django.urls import reverse_lazy
+from usuarios.models import InfoExtra
 
 # Create your views here.
 
@@ -17,6 +22,9 @@ def login(request):
             user = authenticate(username=usuario, password=contrasenia)
             
             app_login(request, user)
+            
+            InfoExtra.objects.get_or_create(user=user)
+            
             return redirect('inicio:inicio')
         else:
             return render(request, 'usuarios/login.html', {'formulario': formulario})    
@@ -37,3 +45,30 @@ def registrarse(request):
     
     formulario = FormularioCreacionUsuarios()
     return render(request, 'usuarios/registro.html', {'formulario': formulario})
+
+
+@login_required
+def editar_perfil(request):
+    
+    
+    info_extra_user = request.user.infoextra
+    
+    if request.method == 'POST':
+        formulario = FormularioEditarDatos(request.POST, request.FILES, instance=request.user)
+        if formulario.is_valid():
+            
+            avatar = formulario.cleaned_data.get('avatar')
+            if avatar:
+                info_extra_user.avatar = avatar
+                info_extra_user.save()
+            formulario.save()
+            return redirect('inicio:inicio')
+    else:
+            formulario = FormularioEditarDatos(initial={'avatar': request.user.infoextra.avatar}, instance=request.user)    
+    
+    return render(request, 'usuarios/edicion_perfil.html', {'formulario': formulario})
+
+
+class ModificarPassword(LoginRequiredMixin, PasswordChangeView):
+    template_name = 'usuarios/modificar_password.html'
+    success_url = reverse_lazy('usuarios:editar_perfil')
